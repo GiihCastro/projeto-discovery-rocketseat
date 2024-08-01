021f5bd30ba76980caa2a811bf1b886b97ee0556
Giovanna Castro <giovanna190506@gmail.com>
2024-08-01 20:47:35 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("aly", "Foto de Mayk Brito com fundo roxo e azul para perfil")
+  } else {
+    //se tiver sem light mode, manter a imagem original
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("aly", "Foto de Mayk Brito com fundo amarelo para perfil")
+  }
+}
